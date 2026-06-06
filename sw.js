13c59724d49e876e9359567dda038e5c04496873
HarryFox11 <harrisonmichaelfox@outlook.com>
2026-06-06 13:44:18 -0400

    update with kill switch

--- sw.js
+++ sw.js
@@ -1,11 +1,9 @@
-// Nuclear reset - clears everything and unregisters itself
+// Empty service worker - unregisters itself and clears all caches
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys()
       .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
       .then(() => self.registration.unregister())
   );
 });
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
